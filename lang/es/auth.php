63f3ad67a5ad9e7f4531b4c5fbf8579ff9edee58
Jostin <jostin@example.com>
2026-05-20 17:59:02 -0600

    lenguaje

---
+++ lang/es/auth.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Líneas de idioma de autenticación
+    |--------------------------------------------------------------------------
+    |
+    | Las siguientes líneas se usan durante la autenticación para varios
+    | mensajes que necesitamos mostrar al usuario.
+    |
+    */
+
+    'failed' => 'Las credenciales ingresadas no coinciden con nuestros registros.',
+    'password' => 'La contraseña proporcionada es incorrecta.',
+    'throttle' => 'Demasiados intentos de inicio de sesión. Por favor intente de nuevo en :seconds segundos.',
+
+];
